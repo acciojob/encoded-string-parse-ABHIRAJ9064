094570ef2afa69cddc4eb52fe8f9b94a3b946f27
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-03 13:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const parseCode = (str) => {
   // your code here
-};
+  const [firstName, lastName, id] = str.split('0').filter(Boolean);
+  return { firstName, lastName, id };
+}
+
 
 // Do not change the code below
 const str = prompt("Enter str: ");
